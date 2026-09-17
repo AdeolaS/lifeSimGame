67b3845b9181abc0fcc2b5872ed347cdd008efcb
AdeolaS <adeoladesign@hotmail.co.uk>
2026-09-17 20:53:15 +0100

    Initial Setup

---
+++ src/main/java/com/onlyalive/lifeSimGame/LifeSimGameApplication.java
@@ -0,0 +1,13 @@
+package com.onlyalive.lifeSimGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSimGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeSimGameApplication.class, args);
+	}
+
+}
